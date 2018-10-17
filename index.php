d3ca95642d441d1d233c22ddb02435327387b19f
GraemeT <gdthornber@gmail.com>
2018-10-18 01:13:50 +1300

    Fix Safari wrapping bug in header

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 		<div class="inner clearfix">
 			<div class="hgroup">
 				<h1><a href="/"><span class="big">T</span><span>hornberry</span><span class="big">P</span><span>ie</span><span class="suffix">.com</span></a></h1>
-				<h2>Portfolio of Web Developer Graeme Thornber</h2>
+				<h2>Portfolio of Web Developer Graeme&nbsp;Thornber</h2>
 			</div>
 			<div class="nav-wrapper">
 				<nav class="nav-home">
