cb1664c137fde0fb61475f213de5a8c402a7bb90
GraemeT <gdthornber@gmail.com>
2019-01-19 22:18:36 +1300

    Refactoring

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,20 @@
-<header class="flex header">
-	<div class="flex-item logo">
-		<h1 class="title"><a href="/"><span class="big">T</span><span>hornberry</span><span class="big">P</span><span>ie</span><span class="suffix">.com</span></a></h1>
-		<h2 class="tagline">Portfolio of Web Developer Graeme&nbsp;Thornber</h2>
-	</div>
-	<nav class="flex-item nav">
-		<div class="nav-inner">
-			<div id="nav-slider"></div>
-			<ul class="nav-list">
-				<li class="nav-item nav-item--home"><button data-id="home" class="nav-link">Home</button></li>
-				<li class="nav-item nav-item--work"><button data-id="work" class="nav-link">Work</button></li>
-				<li class="nav-item nav-item--about"><button data-id="about" class="nav-link">About</button></li>
-				<li class="nav-item nav-item--contact"><button data-id="contact" class="nav-link">Contact</button></li>
-			</ul>
-		</div>
-	</nav>
+<header class="header wrapper">
+  <div class="flex container">
+  	<div class="logo">
+  		<h1 class="title"><a href="/"><span class="big">T</span><span>hornberry</span><span class="big">P</span><span>ie</span><span class="suffix">.com</span></a></h1>
+  		<h2 class="tagline">Portfolio of Web Developer Graeme&nbsp;Thornber</h2>
+  	</div>
+  	<nav class="nav">
+  		<div class="nav-inner">
+  			<div id="nav-slider"></div>
+  			<ul class="nav-list">
+  				<li class="nav-item nav-item--home"><button data-id="home" class="nav-link">Home</button></li>
+  				<li class="nav-item nav-item--work"><button data-id="work" class="nav-link">Work</button></li>
+  				<li class="nav-item nav-item--about"><button data-id="about" class="nav-link">About</button></li>
+  				<li class="nav-item nav-item--contact"><button data-id="contact" class="nav-link">Contact</button></li>
+  			</ul>
+  		</div>
+  	</nav>
+  </div>
 	<noscript><br /><br />{Please enable Javascript}</noscript>
 </header>
--- includes/home.php
+++ includes/home.php
@@ -1,13 +1,6 @@
 <article class="home article">
-	<div class="inner clearfix">
-		<div class="home-right">
-			<p class="intro"><span class="intro1">My work involves many acronyms,</span> <span class="intro2">such as:</span></p>
-			<h2 class="acronyms">
-				<?php outputAcronyms() ?>
-				<div class="acronimg"></div>
-			</h2>
-		</div>
-		<div class="home-left">
+	<div class="container">
+    <div class="home-intro">
 			<p>I build, deploy and maintain modern web applications for all platforms, coding in a variety of open-source languages like:</p>
 				<ul>
 					<li><b>HTML5</b>: with a focus on SEO, accessibility and web standards using a variety of templating engines and frameworks.</li>
@@ -15,6 +8,12 @@
 					<li><b>Javascript</b>: jQuery expert with strong skills in modern JS: ES6, Node.js, Typescript, Angular, Vue.js, React and React Native.</li>
 					<li><b>PHP & MySQL</b>: CMS and MVC frameworks like Silverstripe, Wordpress, Symfony, Laravel, Drupal, Bolt, Zend and Magento</li>
 				</ul>
+		</div>
+    <div class="home-acronyms">
+			<p class="intro"><span class="intro1">My work involves many acronyms,</span> <span class="intro2">such as:</span></p>
+			<h2 class="acronyms">
+				<?php outputAcronyms() ?>
+			</h2>
 		</div>
 	</div>
 </article>
