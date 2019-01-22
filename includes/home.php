c3b7545c65682259bdcdbe2f059ad472f32a1d1c
GraemeT <gdthornber@gmail.com>
2019-01-23 12:58:54 +1300

    Tidy footer links

--- includes/home.php
+++ includes/home.php
@@ -1,18 +1,8 @@
 <article class="home wrapper">
 	<div class="flex container">
     <div class="home-intro">
-			<p>I offer services dealing with all aspects of the modern digital experience including web sites, mobile apps, <span class="nowrap">e-commerce</span>, digital marketing, SEO, hosting and&nbsp;more.</p>
-			<?php /*
-      <ul>
-				<li><strong>Web sites</strong>: Custom design & development including E-Commerce and CMS frameworks like Wordpress, Magento and Silverstripe.</li>
-        <li><strong>Web apps</strong>: Mini-sites, brochure sites and SPAs built with modern Javascript tools like Angular and React.</li>
-        <li><strong>Mobile apps</strong>: Android & iPhone app development, integration with web sites and apps.</li>
-        <li><strong>Maintenance</strong>: Upgrading, switching hosts, improving performance and accessibility.</li>
-        <li><strong>Digital Marketing</strong>: SEO, social media integration, email marketing with Mailchimp & Campaign Monitor.</li>
-        <li><strong>Hosting</strong>: Variety of hosting solutions from as little as $3 per month.</li>
-			</ul>
-      */ ?>
-      <p>I generally work from home in Whitianga, but I'm currently am available for short-term on-site contracts anywhere in NZ or Australia. <a href="#" class="js-scroll-to-call">Call me</a> for a free consultation or <span class="nowrap">no-obligation</span> quote.</p>
+			<p>With 12 solid years experience as a Web Developer, I&nbsp;offer services dealing with all aspects of the modern digital experience including web sites, mobile apps, <span class="nowrap">e-commerce</span>, digital marketing, integration, search engine optimisation, hosting and&nbsp;more.</p>
+      <p>I generally work from home in Whitianga, but I'm currently am available for short-term on-site contracts&nbsp;anywhere in NZ or Australia. <br><a href="#" class="js-scroll-to-call">Call me</a> for a free consultation or <span class="nowrap">no-obligation</span> quote.</p>
       <p>Latest work:<br><a href="http://place-trace.com" target="_blank">A visual javascript geography quiz</a></p>
 		</div>
     <div class="home-acronyms">
